cb37fba0c8c93c3d4558aade87528d85fd4f855c
{{yansssy8}} <{{janaijemma23800@gmail.com}}>
2023-10-02 20:51:49 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+class Point
+{
+    private double x;
+    private double y;
+    private string name;
+
+    public Point(double x, double y, string name)
+    {
+        this.x = x;
+        this.y = y;
+        this.name = name;
+    }
+
+    public double X
+    {
+        get { return x; }
+    }
+
+    public double Y
+    {
+        get { return y; }
+    }
+
+    public string Name
+    {
+        get { return name; }
+    }
+}
+
+class Figure
+{
+    private Point[] points;
+
+    public Figure(Point A, Point B, Point C)
+    {
+        points = new Point[] { A, B, C };
+    }
+
+    public Figure(Point A, Point B, Point C, Point D)
+    {
+        points = new Point[] { A, B, C, D };
+    }
+
+    public Figure(Point A, Point B, Point C, Point D, Point E)
+    {
+        points = new Point[] { A, B, C, D, E };
+    }
+
+    private double GetSideLength(Point A, Point B)
+    {
+        return Math.Sqrt(Math.Pow(B.X - A.X, 2) + Math.Pow(B.Y - A.Y, 2));
+    }
+
+    public void CalculatePerimeter()
+    {
+        double perimeter = 0;
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            int nextIndex = (i + 1) % points.Length;
+            perimeter += GetSideLength(points[i], points[nextIndex]);
+        }
+
+        Console.WriteLine($"Назва багатокутника: {points.Length}-кут");
+        Console.WriteLine($"Периметр багатокутника: {perimeter}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Point A = new Point(0, 0, "A");
+        Point B = new Point(0, 4, "B");
+        Point C = new Point(3, 0, "C");
+
+        Figure triangle = new Figure(A, B, C);
+        triangle.CalculatePerimeter();
+
+        Point D = new Point(0, 0, "D");
+        Point E = new Point(0, 5, "E");
+        Point F = new Point(4, 0, "F");
+
+        Figure rectangle = new Figure(D, E, F);
+        rectangle.CalculatePerimeter();
+
+        Console.ReadLine();
+    }
+}
